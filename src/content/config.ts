5fc8de4e96b595e9fe933f16481e7cbb8abb5c95
Justyn <wheatleyokok@gmail.com>
2025-03-30 12:43:48 -0400

    Doing the meta heroImage stuff properly

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,7 +23,7 @@ const portfolio = defineCollection({
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		heroImage: z.string(),
 		draft: z.boolean().optional(),
 	}),
 });
