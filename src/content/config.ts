b93ebb05dc700b40d402a2d5ec222eaf7640690c
Justyn <wheatleyokok@gmail.com>
2025-03-30 02:46:24 -0400

    Recreated the dev branch

---
+++ src/content/config.ts
@@ -0,0 +1,50 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		draft: z.boolean().optional(),
+	}),
+});
+
+const portfolio = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		draft: z.boolean().optional(),
+	}),
+});
+
+const archive = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		archiveMedia: z.string().optional(),
+		draft: z.boolean().optional(),
+	}),
+});
+
+export const collections = {
+	'blog': blog,
+	'portfolio': portfolio,
+	'archive': archive,
+  };
